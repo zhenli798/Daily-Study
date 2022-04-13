67d049e767bb158a6842f7f09d4fc40ee3bee281
zhenli798 <798663691@qq.com>
2022-04-13 13:11:50 +0800

    python学习笔记更新

---
+++ Learning of python/basic_learning/04 chinese_zodiac.py
@@ -0,0 +1,13 @@
+# -*- coding = utf-8 -*-
+# @Time: 2022/4/13 11:35
+# @Author: 陌言
+# @File: 04 chinese_zodiac.py
+# @SoftWare: PyCharm
+
+# 记录生肖，根据年份来判断生肖
+chinese_zodiac = '猴鸡狗猪鼠牛虎兔龙蛇马羊'
+
+print('狗' in chinese_zodiac)
+print('狗' not in chinese_zodiac)
+print(chinese_zodiac + 'abcd')
+print(chinese_zodiac * 3)
